bf9e56e6aa59818cf3ac45c440aca3959889595b
bodpunk <bodpunk@mail.ru>
2017-07-21 02:36:20 +0500

    First step

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  $(".about-us-carousel").owlCarousel( {
+    items: 1,
+    margin: 10,
+    slideBy: 1,
+    loop: true,
+    nav: true,
+    navText: ["",""],
+    autoplay: true
+  });
+});
+
+$(document).ready(function(){
+  $(".our-residents-carousel").owlCarousel( {
+    items: 1,
+    margin: 10,
+    slideBy: 1,
+    loop: true,
+    nav: true,
+    navText: ["",""],
+    autoplay: true
+  });
+});
